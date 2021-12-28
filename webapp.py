6bdd9f228fe7e80c1b2d88cf44434fdaf07192f7
Ben Murphy <benmurphy7@gmail.com>
2021-12-28 15:22:44 -0500

    Create chart during second step

--- webapp.py
+++ webapp.py
@@ -1,4 +1,5 @@
 import gevent.monkey
+
 gevent.monkey.patch_all()
 
 import twitch
@@ -19,7 +20,7 @@ app = Flask(__name__)
 @app.route('/', methods=('GET', 'POST'))
 def index():
     if request.method == 'POST':
-        #request.args.get('a', 0, type=int)
+        # request.args.get('a', 0, type=int)
         video_id = util.parse_video_id(request.form['video_id'])
         return display_video_info(video_id)
 
@@ -27,12 +28,10 @@ def index():
         return render_template('index.html')
 
 
-@app.route('/chart', methods=('GET', 'POST'))
-def chart():
+@app.route('/submit', methods=('GET', 'POST'))
+def submit():
     if request.method == 'POST':
         video_id = util.parse_video_id(request.form['video_id'])
-
-        video_info = {}
         valid_id = collect.update_video_info(video_id)
         video: twitch.Helix.video = collect.video_info
 
@@ -40,20 +39,40 @@ def chart():
             print('Inavlid Video ID')
             return
 
-        video_info['title'] = video.title
-        video_info['user_name'] = video.user_name
-        video_info['duration'] = util.space_timestamp(video.duration)
+        video_info = {
+            'id': video_id,
+            'title': video.title,
+            'user_name': video.user_name,
+            'duration': util.space_timestamp(video.duration)
+        }
 
-        style_str = "<style>h2 {text-align: center;}</style>"
-        chart_title = "<h2>{} - {}</h2>".format(video.user_name, video.title)
-
-        html_str = render_chart(video_id)
-
-        html_str = style_str + chart_title + html_str
+        return render_template('video_analysis.html', video_info=video_info)
 
 
-    #return render_template('chart_view.html', video_info=video_info)
-    return render_template_string(html_str,video_info=video_info)
+@app.route('/chart/<video_id>', methods=('GET', 'POST'))
+def chart(video_id):
+    valid_id = collect.update_video_info(video_id)
+    video: twitch.Helix.video = collect.video_info
+
+    if not video_id or not valid_id:
+        print('Inavlid Video ID')
+        return
+
+    video_info = {
+        'id': video_id,
+        'title': video.title,
+        'user_name': video.user_name,
+        'duration': util.space_timestamp(video.duration)
+    }
+
+    style_str = "<style>h2 {text-align: center;}</style>"
+    chart_title = "<h2>{} - {}</h2>".format(video.user_name, video.title)
+
+    chart_html_str = render_chart(video_id)
+    chart_html_str = style_str + chart_title + chart_html_str
+
+    # return render_template('chart_view.html', video_info=video_info)
+    return render_template_string(chart_html_str, video_info=video_info)
 
 
 def display_video_info(video_id):
@@ -69,8 +88,6 @@ def display_video_info(video_id):
     video_info['user_name'] = video.user_name
     video_info['duration'] = util.space_timestamp(video.duration)
 
-
-
     return render_template('video_analysis.html', video_info=video_info)
 
 
@@ -85,7 +102,7 @@ def display_emotes(video_id):
 
     try:
         if urls:
-            #self.update_status("Downloading images ...")
+            # self.update_status("Downloading images ...")
             images.get_images(urls)
     except Exception as e:
         print(e)
@@ -99,7 +116,7 @@ def render_chart(video_id, filter_list=[]):
     if invalid is None:
         html_str = analyze.plot_video_data(collect.video_info, data, valid, stats, 50, 10, True)
     else:
-       print("Error: ' {} ' is not a valid filter".format(invalid))
+        print("Error: ' {} ' is not a valid filter".format(invalid))
 
     return html_str
 
@@ -110,6 +127,6 @@ if __name__ == '__main__':
     # on the local development server.
     context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
     context.verify_mode = ssl.CERT_REQUIRED
-    #context.load_verify_locations("ca.crt")
+    # context.load_verify_locations("ca.crt")
     context = ('twitchcrops_tv.crt', 'twitchcrops_tv.key')  # certificate and key files
     app.run(host='0.0.0.0', port=443, ssl_context=context)
